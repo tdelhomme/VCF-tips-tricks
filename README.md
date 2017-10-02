ad1e869784f91a4105ec3a7e5cfbddcab579cf71
tdelhomme <delhommet@students.iarc.fr>
2017-10-02 13:51:39 +0200

    add input parameters in cumulative plot

--- README.md
+++ README.md
@@ -11,7 +11,9 @@ A good test on the quality of the variant calling is to have a look on these dis
 
 [Here](https://github.com/tdelhomme/VCF-tips-tricks/blob/master/code/cumulative_plot_vcf.r) is a R code to plot the distribution of substitutions for multiple classes of allelic fractions.  
 The input parameters are :
-  * __input_VCF__ (no default)
+  * __input_vcf__ (no default)
   * __min_AF__ (default=0)
   * __max_AF__ (default=1)
   * __nCPU__ (default=1)
+  * __nb_classes__ (default=100)
+  * __AF_field__ (default=AF)
--- code/cumulative_plot_vcf.r
+++ code/cumulative_plot_vcf.r
@@ -1,15 +1,25 @@
 library(VariantAnnotation)
 
-input_vcf = "input.vcf.gz"
+# parse input parameters
 
-allelic_fractions_field = "AF"
-af_length = 100
+args <- commandArgs(TRUE)
+parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
+argsL <- as.list(as.character(as.data.frame(do.call("rbind", parseArgs(args)))$V2))
+names(argsL) <- as.data.frame(do.call("rbind", parseArgs(args)))$V1
+args <- argsL;rm(argsL)
+
+if(is.null(args$input_vcf)) {stop("no input VCF file")} else {input_vcf = args$input_vcf}
+if(is.null(args$nb_classes)) {nb_classes = 100)} else {nb_classes = as.numeric(args$nb_classes)}
+if(is.null(args$AF_field)) {AF_field = "AF")} else {AF_field = args$AF_field)}
+if(is.null(args$min_AF)) {min_AF = 0)} else {min_AF = as.numeric(args$min_AF)}
+if(is.null(args$max_AF)) {max_AF = 1)} else {max_AF = as.numeric(args$max_AF)}
+if(is.null(args$nCPU)) {nCPU = 1)} else {nCPU = as.numeric(args$nCPU)}
 
 # contruct a prop barplot for substitutions stratified by AF
 
 vcf <- open(VcfFile(input_vcf,  yieldSize=1000000))
 vcf_chunk = readVcf(vcf, "hg19")
-AF_df = as.data.frame(geno(vcf_chunk, allelic_fractions_field))
+AF_df = as.data.frame(geno(vcf_chunk, AF_field))
 AF_df$ref = as.character(ref(vcf_chunk))
 AF_df$alt = as.character(unlist(alt(vcf_chunk)))
 AF_df_snv = AF_df[which(AF_df$ref %in% c("A","G","C","T") & AF_df$alt %in% c("A","G","C","T")),]
@@ -23,9 +33,9 @@ AF_df_snv[which(AF_df_snv$subst == "GT"),"subst"] = "CA"
 AF_df_snv[which(AF_df_snv$subst == "GC"),"subst"] = "CG"
 AF_df_snv[which(AF_df_snv$subst == "GA"),"subst"] = "CT"
 
-AF_classes = seq(0, 1, length = af_length + 1)
+AF_classes = seq(0, 1, length = nb_classes + 1)
 
-l = lapply(1:af_length, function(i){
+l = lapply(1:nb_classes, function(i){
   nb_af_ok = apply(AF_df_snv[,1:(ncol(AF_df_snv)-3)], 1, function(x) {
     sum(x>AF_classes[i] & x<=AF_classes[i+1], na.rm = T)
   })
@@ -36,14 +46,16 @@ l = lapply(1:af_length, function(i){
   t # return the result
 })
 
-prop_subst = matrix(unlist(l), ncol = af_length, byrow = F)
+prop_subst = matrix(unlist(l), ncol = nb_classes, byrow = F)
 rownames(prop_subst) = c("AG","AC","AT","CA","CG","CT")
+
+# output the resulting PDF plot
 
 pdf("substitutions_proportion_by_AF.pdf",10,5)
 par(lwd = 0.25)
 layout(matrix(c(1,1,1,1,2), 1, 5, byrow = TRUE))
 barplot(prop_subst, col = rainbow(length(rownames(prop_subst))), space=0)
-axis(1,labels=AF_classes, at=c(0, seq(1:af_length)))
+axis(1,labels=AF_classes, at=c(0, seq(1:nb_classes)))
 plot(1, type="n", axes=FALSE, xlab="", ylab="")
 legend("topright", legend=c("AG","AC","AT","CA","CG","CT"), col = rainbow(length(rownames(prop_subst))), pch=19)
 dev.off()
